4b21f475bcad48123253ce66ab84c7849c0fc06d
krist <krist@LAPTOP-HIQLHLL6>
2022-11-28 17:40:48 +0500

    Добавьте файлы проекта.

---
+++ Lab 23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Factor(n);
+            Console.ReadKey();
+        }
+        static void Fac(int n)
+        {
+            int s = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                s *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(s);
+        }
+        static async void Factor( int n)
+        {
+            await Task.Run(() => Fac(n));
+        }
+    }
+}
